afbdb19c0977313bd9d8a891e2a6717fe7a8ce52
Anas <anass_hamzaoui@hotmail.com>
2023-12-30 13:01:48 +0100

    Ajout des appels dans App.java

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -2,6 +2,18 @@ package ma.ehei.calculatrice;
 
 public class App {
 	public static void main(String[] args) {
+
 		System.out.println(Calculatrice.add(5, 5));
+
+		Calculatrice calcul = new Calculatrice();
+
+		int firstResult = calcul.multiplication(5, 3);
+		int secondResult = calcul.multiplication(10, 2);
+
+		System.out.println("first result : " + firstResult);
+		System.out.println("second result : " + secondResult);
+
+
+
 	}
 }
